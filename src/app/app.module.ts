189d33f70cad46daaa35dc39e00d3b7e7e34e667
Stefano Corra <stefano.corra@hotmail.com>
2017-06-21 23:35:05 -0400

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './start/app.component';
+
+import { NavComponent } from './shared/navbar.component';
+
 @NgModule({
     imports: [
         BrowserModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        NavComponent      
     ],
     bootstrap: [ AppComponent ]
 })
