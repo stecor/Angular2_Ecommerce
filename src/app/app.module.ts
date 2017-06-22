32a19fc3a0c155763332ac02253e193b630f0e49
Stefano Corra <stefano.corra@hotmail.com>
2017-06-22 01:10:47 -0400

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './start/app.component';
 
 import { NavComponent } from './shared/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { AppRoutingModule } from './shared/app.routing';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule,
     ],
     declarations: [
         AppComponent,
-        NavComponent      
+        NavComponent,
+        HomeComponent,
+        ErrorComponent,      
     ],
     bootstrap: [ AppComponent ]
 })
